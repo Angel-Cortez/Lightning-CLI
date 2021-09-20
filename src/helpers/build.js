0d81a0147b32da05dea074ec1be2d868d6ba7a81
Angel Cortez <angel.cortez@nbcuni.com>
2021-09-20 11:39:49 -0400

    • Test LNG_BUNDLER

--- src/helpers/build.js
+++ src/helpers/build.js
@@ -140,6 +140,7 @@ const readJson = fileName => {
 }
 
 const bundleEs6App = (folder, metadata, options = {}) => {
+  console.log("Test LNG_BUNDLER: " + process.env.LNG_BUNDLER)
   if (process.env.LNG_BUNDLER === 'esbuild') {
     return buildAppEsBuild(folder, metadata, 'es6', options)
   } else {
